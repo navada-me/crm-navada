eec569ea23ede1464cc5015b127b50b915c4d910
navada-me <permindersingh93@gmail.com>
2020-10-20 18:55:21 -0400

    data dump

--- crm_navada/settings.py
+++ crm_navada/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'crm_navada.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Deployment database setup uncomment it when deploying
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# # Deployment database setup uncomment it when deploying
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 
 
